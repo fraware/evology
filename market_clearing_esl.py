93e8c93c45075577dfe2e4bbbe8e30ac509605f3
aymericvie <aymericvie78@gmail.com>
2021-06-09 11:00:23 +0200

    failed attempts on clearing

--- market_clearing_esl.py
+++ market_clearing_esl.py
@@ -1,3 +1,52 @@
+# import esl
+# from esl.law import property
+# from esl.simulation import identity
+# from esl.economics import price
+# from esl.economics import currencies
+# from esl.economics.markets import quote
+# from esl.economics.markets.walras import excess_demand_model, differentiable_order_message
+
+# def solve(excess_demand_functions: list):
+#     i  = identity([0, 1])
+#     p  = property(i)
+#     initial_price = price(int(1.23 * currencies.USD.denominator), currencies.USD)
+#     initial_quote = quote(initial_price)
+#     market_agent = esl.simulation.identity([0])
+#     excess_demand_functions = []
+#     model = excess_demand_model({p: initial_quote})
+
+#     # for i, ed in enumerate(my_excess_demand_functions):
+
+#     #     order = ed(esl.simulation.identity([i+1]), market_agent, 0, 0)
+
+#     #     excess_demand_functions.append(order) 
+
+#     model.excess_demand_functions = excess_demand_functions
+#     multipliers = model.compute_clearing_quotes() 
+
+#     prices = []
+
+#     for k, v in multipliers.items():
+
+#         prices.append(price(round(float(initial_price) * v * currencies.USD.denominator), currencies.USD))
+
+
+#     del model
+#     return prices
+
+# def aggregate_ed1(x):
+#     return 3 * x - 10
+
+# def aggregate_ed2(x):
+#     return 5 * x - 7
+
+# aggregate_ed = [aggregate_ed1, aggregate_ed2]
+
+# prices = solve(aggregate_ed)
+# print(prices)
+
+''' As understood on 09/06: ed1, ed2 would have to be orders, not functions '''
+
 import esl
 from esl.law import property
 from esl.simulation import identity
@@ -6,52 +55,20 @@ from esl.economics import currencies
 from esl.economics.markets import quote
 from esl.economics.markets.walras import excess_demand_model, differentiable_order_message
 
-def solve(excess_demand_functions: list):
-    i  = identity([0, 1])
-    p  = property(i)
-    initial_price = price(int(1.23 * currencies.USD.denominator), currencies.USD)
-    initial_quote = quote(initial_price)
-    market_agent = esl.simulation.identity([0])
-    excess_demand_functions = []
-    model = excess_demand_model({p: initial_quote})
+class my_order(differentiable_order_message):
+    def __init__(self, sender, recipient, sent, received):
+        super().__init__(sender, recipient, sent, received)
+        # TODO (Maarten): this should be exposed through base class
+        self.sender = sender
 
-    # for i, ed in enumerate(my_excess_demand_functions):
+    def excess_demand(self, quotes) -> float:
+        print(f"The Walrasian price setter suggests the following prices: {quotes}")
+        ed = {k: ((i+3.) - (float(v[0]) * v[1])) for i,  (k, v) in enumerate(quotes.items())}
+        print(f"Agent {self.sender}'s excess demand at these prices is: {ed}")
+        return ed
+    
+# order = my_order(trader_agent, market_agent, current_time, current_time)
 
-    #     order = ed(esl.simulation.identity([i+1]), market_agent, 0, 0)
-
-    #     excess_demand_functions.append(order) 
-
-    model.excess_demand_functions = excess_demand_functions
-    multipliers = model.compute_clearing_quotes() 
-
-    prices = []
-
-    for k, v in multipliers.items():
-
-        prices.append(price(round(float(initial_price) * v * currencies.USD.denominator), currencies.USD))
-
-
-    del model
-    return prices
-
-def aggregate_ed1(x):
-    return 3 * x - 10
-
-def aggregate_ed2(x):
-    return 5 * x - 7
-
-aggregate_ed = [aggregate_ed1, aggregate_ed2]
-
-prices = solve(aggregate_ed)
-print(prices)
-
-import esl
-from esl.law import property
-from esl.simulation import identity
-from esl.economics import price
-from esl.economics import currencies
-from esl.economics.markets import quote
-from esl.economics.markets.walras import excess_demand_model, differentiable_order_message
 def solve(my_excess_demand_functions: list):
     
      i  = identity([0, 1])
@@ -66,6 +83,7 @@ def solve(my_excess_demand_functions: list):
          order = ed(esl.simulation.identity([i+1]), market_agent, 0, 0)
          excess_demand_functions.append(order) 
      model.excess_demand_functions = excess_demand_functions
+     print(excess_demand_functions)
      multipliers = model.compute_clearing_quotes() 
      prices = []
      for k, v in multipliers.items():
