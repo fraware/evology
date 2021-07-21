3d191787282a1af82a00bea68e05c9692084772f
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2021-07-21 12:01:09 +0200

    structure update

---
+++ market/untitled2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 21 12:00:48 2021
+
+@author: Administrator
+"""
+
+market test
---
+++ structure.py
@@ -0,0 +1,44 @@
+"""
+ ## Market block 
+ 
+The market folder contains the market functions.
+1) Initialise market
+2) Apply interest, dividends and reinvestment
+3) Update wealth and profit
+-) Compute excess demand functions
+-) Market clearing algorithm
+-) Apply excess demand 
+-) Market parameters?
+
+ ## Strategy block
+A) Compute trading signal Phi
+
+
+## Testing block
+A testing folder that has a script for every script in the code, to test it 
+does work well and normally.
+
+
+
+ ## Learning block 
+Organised in different cases depending on the learning algorithm we use.
+- No learning - Maarten's model
+- Genetic Programming (DEAP)
+- Genetic algorithm (3 types, parametric evolution)
+ 
+
+## Experiment block
+The experiment folder contains folder experiments, one for each.
+An experiment uses a market parameter, a learning parameter, a script.
+Data and figures should be saved in separate folders for each experiment.
+
+
+
+
+#### Next tasks
+
+DEAP is constraining our job for the EAs. Why? Because of GP.
+Should we start coding for the most constraining thing first? So that we can 
+then have a structure as generalised as possible.
+
+"""
