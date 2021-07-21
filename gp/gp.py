a282241b4e036286abb0714149cf2cbc4649e6dc
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2021-07-21 17:09:11 +0200

    gp pygraphviz solved

--- gp/gp.py
+++ gp/gp.py
@@ -10,25 +10,29 @@ from deap import creator
 from deap import tools
 from deap import gp
 
+import matplotlib.pyplot as plt
+import networkx as nx
+import graphviz
+import pygraphviz as pgv 
+from networkx.drawing.nx_agraph import graphviz_layout
+#in Conda prompt conda install -c alubbock pygraphviz run in admin mode
+
+
 # Define new functions
 def protectedDiv(left, right):
-    with numpy.errstate(divide='ignore',invalid='ignore'):
-        x = numpy.divide(left, right)
-        if isinstance(x, numpy.ndarray):
-            x[numpy.isinf(x)] = 1
-            x[numpy.isnan(x)] = 1
-        elif numpy.isinf(x) or numpy.isnan(x):
-            x = 1
-    return x
+    try:
+        return left / right
+    except ZeroDivisionError:
+        return 1
 
 pset = gp.PrimitiveSet("MAIN", 1)
-pset.addPrimitive(numpy.add, 2, name="vadd")
-pset.addPrimitive(numpy.subtract, 2, name="vsub")
-pset.addPrimitive(numpy.multiply, 2, name="vmul")
+pset.addPrimitive(operator.add, 2)
+pset.addPrimitive(operator.sub, 2)
+pset.addPrimitive(operator.mul, 2)
 pset.addPrimitive(protectedDiv, 2)
-pset.addPrimitive(numpy.negative, 1, name="vneg")
-pset.addPrimitive(numpy.cos, 1, name="vcos")
-pset.addPrimitive(numpy.sin, 1, name="vsin")
+pset.addPrimitive(operator.neg, 1)
+pset.addPrimitive(math.cos, 1)
+pset.addPrimitive(math.sin, 1)
 # pset.addEphemeralConstant("rand101", lambda: random.randint(-1,1))
 pset.renameArguments(ARG0='x')
 
@@ -41,37 +45,55 @@ toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.ex
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("compile", gp.compile, pset=pset)
 
-samples = numpy.linspace(-1, 1, 10000)
-values = samples**4 + samples**3 + samples**2 + samples
-
-def evalSymbReg(individual):
+def evalSymbReg(individual, points):
     # Transform the tree expression in a callable function
     func = toolbox.compile(expr=individual)
-    # Evaluate the sum of squared difference between the expression
-    # and the real function values : x**4 + x**3 + x**2 + x 
-    diff = numpy.sum((func(samples) - values)**2)
-    return diff,
+    # Evaluate the mean squared error between the expression
+    # and the real function : x**4 + x**3 + x**2 + x
+    sqerrors = ((func(x) - x**4 - x**3 - x**2 - x)**2 for x in points)
+    return math.fsum(sqerrors) / len(points),
 
-toolbox.register("evaluate", evalSymbReg)
+toolbox.register("evaluate", evalSymbReg, points=[x/10. for x in range(-10,10)])
 toolbox.register("select", tools.selTournament, tournsize=3)
 toolbox.register("mate", gp.cxOnePoint)
 toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
-toolbox.register('mutate', gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
 
-pop = toolbox.population(n=3)
-print(pop)
+toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
+toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
 
-import matplotlib.pyplot as plt
-import networkx
-import pygraphviz as pgv
-nodes, edges, labels = gp.graph(expr)
-graph = networkx.Graph()
+def main():
+    random.seed(318)
+
+    pop = toolbox.population(n=300)
+    hof = tools.HallOfFame(1)
+    
+    stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
+    stats_size = tools.Statistics(len)
+    mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
+    mstats.register("avg", numpy.mean)
+    mstats.register("std", numpy.std)
+    mstats.register("min", numpy.min)
+    mstats.register("max", numpy.max)
+
+    pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.1, 40, stats=mstats,
+                                   halloffame=hof, verbose=True)
+    # print log
+    return pop, log, hof
+
+pop, log, hof = main()
+bests = tools.selBest(pop, k=1)
+
+nodes, edges, labels = gp.graph(bests[0])
+graph = nx.Graph()
 graph.add_nodes_from(nodes)
 graph.add_edges_from(edges)
-pos = graphviz_layout(graph, prog='dot')
+pos = graphviz_layout(graph, prog = 'dot') #run dot -c in conda prompt
 plt.figure(figsize=(7,7))
-networkx.draw_networkx_nodes(graph, pos, node_size=900, node_color="w")
-networkx.draw_networkx_edges(graph, pos)
-networkx.draw_networkx_labels(graph, pos, labels)
+nx.draw_networkx_nodes(graph, pos, node_size=900, node_color="w")
+nx.draw_networkx_edges(graph, pos)
+nx.draw_networkx_labels(graph, pos, labels)
 plt.axis("off")
 plt.show()
+
+print(bests[0])
