d97c647fd65b179a7c58bf6346366a603ded9b67
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2021-07-25 12:41:57 +0200

    deap ind.attribute

---
+++ gp/deapgp_creating_type.py
@@ -0,0 +1,222 @@
+import operator
+import math
+import random
+
+import numpy
+
+from deap import algorithms
+from deap import base
+from deap import creator
+from deap import tools
+from deap import gp
+
+import matplotlib.pyplot as plt
+import networkx as nx
+import graphviz
+import pygraphviz as pgv 
+from networkx.drawing.nx_agraph import graphviz_layout
+#in Conda prompt conda install -c alubbock pygraphviz run in admin mode
+
+
+# Define new functions
+def protectedDiv(left, right):
+    try:
+        return left / right
+    except ZeroDivisionError:
+        return 1
+
+pset = gp.PrimitiveSet("MAIN", 1)
+pset.addPrimitive(operator.add, 2)
+pset.addPrimitive(operator.sub, 2)
+pset.addPrimitive(operator.mul, 2)
+pset.addPrimitive(protectedDiv, 2)
+pset.addPrimitive(operator.neg, 1)
+pset.addPrimitive(math.cos, 1)
+pset.addPrimitive(math.sin, 1)
+# pset.addEphemeralConstant("rand101", lambda: random.randint(-1,1))
+pset.renameArguments(ARG0='x')
+
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin, 
+               types=None, tsf = None)
+
+toolbox = base.Toolbox()
+toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=2)
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+toolbox.register("compile", gp.compile, pset=pset)
+
+def evalSymbReg(individual, points):
+    # Transform the tree expression in a callable function
+    func = toolbox.compile(expr=individual)
+    # Evaluate the mean squared error between the expression
+    # and the real function : x**4 + x**3 + x**2 + x
+    sqerrors = ((func(x) - x**4 - x**3 - x**2 - x)**2 for x in points)
+    return math.fsum(sqerrors) / len(points),
+
+# def evalSymbReg(individual, points):
+#     # Transform the tree expression in a callable function
+#     func = toolbox.compile(expr=individual)
+#     # Evaluate the mean squared error between the expression
+#     # and the real function : x**4 + x**3 + x**2 + x
+#     sqerrors = ((func(x) - x**4 - x**3 - x**2 - x)**2 for x in points)
+#     return 0,
+
+def true_evalSymbReg(individual, points):
+    # Transform the tree expression in a callable function
+    func = toolbox.compile(expr=individual)
+    # Evaluate the mean squared error between the expression
+    # and the real function : x**4 + x**3 + x**2 + x
+    sqerrors = ((func(x) - x**4 - x**3 - x**2 - x)**2 for x in points)
+    return math.fsum(sqerrors) / len(points),
+
+toolbox.register("true_evaluate", true_evalSymbReg, points=[x/10. for x in range(-10,10)])
+
+toolbox.register("evaluate", evalSymbReg, points=[x/10. for x in range(-10,10)])
+toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("mate", gp.cxOnePoint)
+toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
+toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+
+toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
+toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
+
+def main():
+    random.seed(318)
+
+    hof = tools.HallOfFame(1)
+    pop_size = 50
+    halloffame = hof
+    population = toolbox.population(n=pop_size)
+    cxpb = 0.5
+    ngen =100
+    mutpb = 0.1
+    
+    verbose=True
+    
+    stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
+    stats_size = tools.Statistics(len)
+    mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
+    mstats.register("avg", numpy.mean)
+    mstats.register("std", numpy.std)
+    mstats.register("min", numpy.min)
+    mstats.register("max", numpy.max)
+    stats=mstats
+    logbook = tools.Logbook()
+    logbook.header = ['gen', 'nevals'] + (stats.fields if stats else [])
+
+    # Evaluate the individuals with an invalid fitness
+    invalid_ind = [ind for ind in population if not ind.fitness.valid]
+    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+    # print("fitnesses")
+    # print(list(fitnesses))
+    for ind, fit in zip(invalid_ind, fitnesses):
+        ind.fitness.values = fit
+        
+
+    if halloffame is not None:
+        halloffame.update(population)
+
+    record = stats.compile(population) if stats else {}
+    logbook.record(gen=0, nevals=len(invalid_ind), **record)
+    if verbose:
+        print(logbook.stream)
+
+    # Begin the generational process
+    for gen in range(1, ngen + 1):
+        # Select the next generation individuals
+        
+        offspring = toolbox.select(population, len(population))
+
+        # Vary the pool of individuals
+        offspring = [toolbox.clone(ind) for ind in offspring]
+
+        for i in range(1, len(offspring), 2):
+            if random.random() < cxpb:
+                offspring[i - 1], offspring[i] = toolbox.mate(offspring[i - 1],
+                                                          offspring[i])
+            del offspring[i - 1].fitness.values, offspring[i].fitness.values
+
+        for i in range(len(offspring)):
+            if random.random() < mutpb:
+                offspring[i], = toolbox.mutate(offspring[i])
+                del offspring[i].fitness.values
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        # Update the hall of fame with the generated individuals
+        if halloffame is not None:
+            halloffame.update(offspring)
+
+        # Replace the current population by the offspring
+        population[:] = offspring
+        
+        if gen >= ngen:
+            fitnesses = toolbox.map(toolbox.true_evaluate, population)
+            for ind, fit in zip(population, fitnesses):
+                ind.fitness.values = fit  
+
+        # Append the current generation statistics to the logbook
+        record = stats.compile(population) if stats else {}
+        logbook.record(gen=gen, nevals=len(invalid_ind), **record)
+        if verbose:
+            print(logbook.stream)
+
+        
+    # print log
+    return population, logbook, hof
+
+pop, log, hof = main()
+bests = tools.selBest(pop, k=1)
+
+nodes, edges, labels = gp.graph(bests[0])
+graph = nx.Graph()
+graph.add_nodes_from(nodes)
+graph.add_edges_from(edges)
+pos = graphviz_layout(graph, prog = 'dot') #run dot -c in conda prompt
+plt.figure(figsize=(7,7))
+nx.draw_networkx_nodes(graph, pos, node_size=900, node_color="w")
+nx.draw_networkx_edges(graph, pos)
+nx.draw_networkx_labels(graph, pos, labels)
+plt.axis("off")
+plt.show()
+
+def plot_graph(tree):
+    nodes, edges, labels = gp.graph(tree)
+    graph = nx.Graph()
+    graph.add_nodes_from(nodes)
+    graph.add_edges_from(edges)
+    pos = graphviz_layout(graph, prog = 'dot') #run dot -c in conda prompt
+    plt.figure(figsize=(7,7))
+    nx.draw_networkx_nodes(graph, pos, node_size=900, node_color="w")
+    nx.draw_networkx_edges(graph, pos)
+    nx.draw_networkx_labels(graph, pos, labels)
+    plt.axis("off")
+    plt.show()
+
+
+# print("Pop")
+# for i in range(len(pop)):
+#     print(pop[i])
+#     plot_graph(pop[i])
+
+print("Best ")
+print(bests[0])
+
+for ind in pop:
+    ind.types = random.random()
+for ind in pop:
+    print(ind.types)
+    
+def func(x):
+    return x**2
+for ind in pop:
+    ind.tsf = func 
+for ind in pop:
+    print(ind.tsf)
+    print(ind.tsf(2) + ind.types)
+
--- structure.py
+++ structure.py
@@ -59,7 +59,7 @@ Objects
 
 Strategy object
 for GP: 1*n of functions (TSF will be complete copy)
-for GA: one-parametric? Two-parametric?
+for GA: one-parametric? Two-parametric? lets say one and keep it simple.
 
 0) Initialisation of market, initialisation of population. 
     ## Requires a first wealth comput.
@@ -85,5 +85,67 @@ C) Evolution of genotype
         b. Adaptation
     
     strategy, balance_sheet, types, performance = evolution(strategy, balance_sheet, types, performance)
+
+
+
+MODELS
+- A simple GA with 3 strategy types: TF (time horizon), VI (value), NT (value), close to Maarten's. 
+- A GP model (much more open and interesting and deep), varying terminals/primitives
+- Maybe also: novelty search, MAP elites, elitist GA/GP
+
+AXES OF ANALYSIS
+- ALife & ecology dynamics, predator-prey, interactions...
+- Open-Ended Evolution: is it OE? Learning model? Search space size? 
+    Measures of complexity/phenotype diversity?
+- Market efficiency over time
+- Market stylized facts
+- Market dynamics and impact of evolution (off/on/amount/modalities (ex: high mutation low cross))
+- EC analysis: characterising the fitness landscape
+- Systemic risk and FED activity (just a variation of hypermutation)
+
+
+
+
+For GA: we start with initial distributions
+One important point is how the distribution changes.
+Two main modes of evolution: crossover.
+1- Srategies able to mix with other types (Free GA)
+    Obvious coding issue as we cannot crossover those that easily.
+    If ind A meets B and B is of different type, children C and D will be copies?
+    Or C and D become at random A or B? (potentially CD both A or B)
+    
+    It would be really nice to have a ind.types object, like ind.fitness
+    
+    creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin, 
+                   types=None)
+    for ind in pop:
+        ind.types = random.random()
+    for ind in pop:
+        print(ind.types)
+        
+    And it works!! So we would have something like if ind1.types == ind2.types: [] else []
+    This is a very cool way to store agent characteristics. Should we store all?
+    Such as ind.wealth, ind.cash, ind.loan, ind.asset, ind.profit, naturally into ind.fitness
+    Can we store a function?
+    Wow we can! 
+    def func(x):
+        return x**2
+    for ind in pop:
+        ind.tsf = func
+        for ind in pop:
+            print(ind.tsf)
+            print(ind.tsf(2)) and it works!
+    Do we even need the type/balance_sheet/strategy/performance objects? Maybe not!
+    We could do everything with pop inside deap.
+    
+2- Strategies cannot mix with any type (Fidelity cst GA)
+    Strat change only through hypermutation.
+Check if this choice matters for the model dynamics. 
+
+For hypermutation and reinitialisation:
+1- As a function of current distribution (maybe a bit too stochastic, artificially convergent
+        unless we have a types mutation mechanism.)
+2- As a function of initial probabilities
+    We consider that traders in our market are sampled from a larger population with this distr.
 
 """
