ff91dd88efbb6bdf49bac119f7cd879059c1acfb
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2021-09-24 15:32:18 +0100

    include closing short positions for buy orders

--- code/balance_sheet.py
+++ code/balance_sheet.py
@@ -37,6 +37,7 @@ def calculate_wealth(pop, price):
 
         # Update wealth
         ind.wealth = ind.cash + ind.asset_long * price - ind.loan
+        # The amount due by short selling is equally captured by the margin, hence does not appear here.
     return ind
 
 def calculate_ts_edf(pop, extended_price_history):
@@ -161,19 +162,70 @@ def apply_edv(pop, asset_supply, price):
     num_sell_tf, num_sell_vi, num_sell_nt = 0, 0, 0
     num_buy_tf, num_buy_vi, num_buy_nt = 0, 0, 0
 
-    """
-    # determine the bank of exchanges
+    
+    # determine the amount of desired exchanges
     bank_plus = 0
     bank_minus = 0
     bank_short = 0
     for ind in pop:
         if ind.edv > 0:
-            bank_plus += 1
+            bank_plus += ind.edv
         if ind.edv < 0:
             # If the agent sells from inventory: bank_minus += 1
-            # If the agent short sells: bank_short += 1
-            """
+            if ind.edv <= ind.asset_long: # If the agent has enough assets to execute the order
+                bank_minus += ind.edv
+            if ind.edv > ind.asset_long: # If the agent does not have enough assets to execute the order:
+                bank_minus += ind.asset_long
+                bank_short += (ind.edv - ind.asset_long) 
+    # Now we know how much the population wants to buy, sell or short sell.
 
+
+    """ BUT buying back short positions is unconstrained (only by margin) 
+    It is also equivalent. Pay the price, gain one asset (or lose one asset-liability)
+    """
+
+    # STEP 1 
+    # BUY / Close short positions (unconstrained)
+    # Closing first makes sense to allow possible short sellings later (limit of short position size)
+    # Buying a long / closing a short is here equivalent. We close first to simply the balance_sheets and
+    # emphasize long positions.
+
+    for ind in pop:
+        if ind.edv > 0: # If we want to buy:
+            if ind.asset_short > 0: # if we have short positions to clear:
+                buy_back = min(min(ind.edv, ind.asset_short), (ind.cash + ind.margin) / price) # Decide how much
+                ind.asset_short -= buy_back # Apply the closing of the short position
+                # Apply the cost of closing the position
+                if buy_back * price <= ind.margin: # if we have enough in the margin
+                    ind.margin -= buy_back * price
+                elif buy_back * price > ind.margin: # if we don't have enough in the margin
+                    ind.cash -= (buy_back * price - ind.margin)
+                    ind.margin = 0
+                if ind.asset_short == 0: # If we have no short positions, clear the margin back into cash
+                    ind.cash += ind.margin
+                    ind.margin = 0
+
+    # determine the amount of effective exchanges. This is captured by the buy/sell _factors
+    if bank_plus > bank_minus: # We have more buy orders than sell orders
+        # Priority 1: exchange long positions
+        # We will exchange bank_minus shares: everyone who wants to sell can sell, not everyone who wants can buy
+        buy_factor = bank_minus / bank_plus
+        sell_factor = 1
+    if bank_plus == bank_minus: # We live in a wonderful world
+        buy_factor = 1
+        sell_factor = 1
+    if bank_plus < bank_minus: # We have more sell orders than buy orders.
+        # Exchange bank_plus shares, everyone who wants so can buy, not everyone who wants to sell can sell
+        buy_factor = 1
+        sell_factor = bank_plus / bank_minus
+
+    # Execute buying as it is the most constrained (cash) activity
+    assets_bought = 0
+    while assets_bought < bank_plus * buy_factor: # While we have not executed all feasible buy orders:
+        for ind in pop:
+            if ind.edv > 1
+
+    print(str(assets_bought) + " shares bought today.")
 
     for ind in pop:
         if ind.edv > 0:  # the agent wants to buy
