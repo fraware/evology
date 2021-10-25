84b44514f0ab4cc596553caf6db69c11e468d736
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2021-10-25 12:43:58 +0100

    multiple run options

--- code/run.py
+++ code/run.py
@@ -1,13 +1,26 @@
 from main import *
 import pandas as pd
 import random
+from parameters import *
 
 RANDOM_SEED = random.random()
 
 # def main(mode, MAX_GENERATIONS, PROBA_SELECTION, POPULATION_SIZE, CROSSOVER_RATE, MUTATION_RATE):
 
-""" Replication Maarten's results """
-df, pop = main("between", 12500, 0, 3, 0, 0)
+# """ Replication Maarten's results """
+# df, pop = main("between", 12500, 0, 3, 0, 0)
+# print(df)
+
+# df.to_csv("data/pop3_static.csv")
+
+# """ Multi agent without learning """
+# df, pop = main("between", 12500, 0, 100, 0, 0)
+# print(df)
+
+# df.to_csv("data/pop100_static.csv")
+
+""" Multi agent learning """
+df, pop = main("between", 12500, 1, 100, 0, MUTATION_RATE)
 print(df)
 
-df.to_csv("data/run_data.csv")
+df.to_csv("data/pop100_learning_csv")
