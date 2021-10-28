b652424e8b41dd846db61e621c63b3fd0bf74c55
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2021-10-28 14:18:07 +0100

    add setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,7 @@ import os
 os.system("pip install numpy")
 os.system("pip install matplotlib")
 os.system("pip install deap")
-#os.system("pip install leap_ec")
 os.system("pip install pandas")
-
 os.system("pip install -i https://test.pypi.org/simple/ eslpy==0.0.145")
 
 os.system("pip freeze > requirements.txt")
-
-# In MacOS / terminal
-# python3 -m pip install numpy
-
