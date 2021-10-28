bfe97f9096755615062b6529217b0b3b6ff0fb1d
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2021-10-28 19:00:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ os.system("pip install matplotlib")
 os.system("pip install deap")
 os.system("pip install pandas")
 os.system("pip install -i https://test.pypi.org/simple/ eslpy==0.0.145")
+os.system("pip install tqdm")
 
 os.system("pip freeze > requirements.txt")
