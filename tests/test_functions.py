eb6d5a9abb924c6d110467caa47bba66c7312c00
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2021-11-02 13:08:59 +0000

    update test (rd coord + test on dev push)

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -5,10 +5,13 @@ print(sys.version)
 sys.path.append('./evology/code/')
 from main import *
 import random
+import numpy as np
 random.seed = random.random()
 from parameters import *
 
 # main(mode, MAX_GENERATIONS, PROBA_SELECTION, POPULATION_SIZE, CROSSOVER_RATE, MUTATION_RATE):
+
+
 
 def learning_runs(repetitions, time, agents):
 
@@ -16,7 +19,9 @@ def learning_runs(repetitions, time, agents):
     i = 0
     while i < repetitions:
         try: 
-            df = main("between", time, PROBA_SELECTION, agents, 0, MUTATION_RATE, True)
+            wealth_coordinates = np.random.dirichlet(np.ones(3),size=1)[0]
+            print(wealth_coordinates)
+            df = main("between", time, PROBA_SELECTION, agents, 0, MUTATION_RATE, wealth_coordinates, True)
             # print('Process ' + str(i) + ' ran succesfully.')
         except Exception as e: 
             went_smoothly = False
@@ -36,7 +41,9 @@ def nolearning_runs(repetitions, time, agents):
     i = 0
     while i < repetitions:
         try: 
-            df = main("between", time, 0, agents, 0, 0, True)
+            wealth_coordinates = np.random.dirichlet(np.ones(3),size=1)[0]
+            print(wealth_coordinates)
+            df = main("between", time, 0, agents, 0, 0, wealth_coordinates, True)
             # print('Process ' + str(i) + ' ran succesfully.')
         except Exception as e: 
             went_smoothly = False
