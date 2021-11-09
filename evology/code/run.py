020a18ae4218e33675b2d7e54ac25c112e91ac70
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2021-11-09 17:42:38 +0000

    more results

--- evology/code/run.py
+++ evology/code/run.py
@@ -9,7 +9,7 @@ RANDOM_SEED = random.random()
 wealth_coordinates = [1/3, 1/3, 1/3]
 # wealth_coordinates = [0.1, 0.1, 0.8]
 # NT VI TF
-wealth_coordinates = np.random.dirichlet(np.ones(3),size=1)[0].tolist()
+# wealth_coordinates = np.random.dirichlet(np.ones(3),size=1)[0].tolist()
 print(wealth_coordinates)
 
 # def main(mode, MAX_GENERATIONS, PROBA_SELECTION, POPULATION_SIZE, CROSSOVER_RATE, MUTATION_RATE, wealth_coordinates, tqdm_display):
@@ -20,9 +20,11 @@ def run(POPULATION_SIZE, learning_mode, TIME, wealth_coordinates, tqdm_display):
         df = main("static", TIME, 0, POPULATION_SIZE, 0, 0, wealth_coordinates, tqdm_display)
     if learning_mode == 1:
         df = main("between", TIME, PROBA_SELECTION, POPULATION_SIZE, 0, MUTATION_RATE, wealth_coordinates, tqdm_display)
+    if learning_mode == 2:
+        df = main("between", TIME, PROBA_SELECTION, POPULATION_SIZE, 0, 0, wealth_coordinates, tqdm_display)
     return df
 
-df = run(100, 0, 1000, wealth_coordinates, False)
+df = run(100, 2, 10000, wealth_coordinates, False)
 
 df.to_csv("evology/data/run_data.csv")
 # print(df)
