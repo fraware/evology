701a189845a987b10af759c82e9f532ab306a9a0
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2021-12-09 13:59:50 +0000

    nerf totalb totalS mismatch error

--- evology/code/market.py
+++ evology/code/market.py
@@ -79,7 +79,7 @@ def determine_multiplier(pop, spoils, ToLiquidate):
     if multiplier_sell < 0:
         raise ValueError('Multiplier Sell is negative')
 
-    if abs(total_buy * multiplier_buy - total_sell * multiplier_sell) >= 1:
+    if abs(total_buy * multiplier_buy - total_sell * multiplier_sell) >= (0.01 * total_buy * multiplier_buy):
         print(total_buy * multiplier_buy)
         print(total_sell * multiplier_sell)
         raise ValueError('Total buy * Mul is different from Total sell * Mul by more than 1 (abs difference)')
