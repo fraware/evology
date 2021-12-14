e209c3a1a70ada2615793fc2b17e7071109275ff
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2021-12-14 18:37:09 +0000

    run & analysis

--- evology/code/run.py
+++ evology/code/run.py
@@ -26,7 +26,7 @@ def run(POPULATION_SIZE, learning_mode, TIME, wealth_coordinates, tqdm_display, 
 
     return df, pop
 
-df,pop = run(100, 0, 20000, wealth_coordinates, tqdm_display=False, reset_wealth=False)
+df,pop = run(100, 0, 10000, wealth_coordinates, tqdm_display=False, reset_wealth=False)
 df.to_csv("evology/data/run_data.csv")
 print(df)
 
