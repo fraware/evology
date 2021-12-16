37210948ec5fac2117249212540192bd411e62b2
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2021-12-16 18:23:29 +0000

    Multiprocessing: exploitable, numpy output

--- evology/code/EvologyMP.py
+++ evology/code/EvologyMP.py
@@ -1,19 +1,13 @@
-# import concurrent.futures
 import multiprocessing as mp
 from main import *
 import time
-# import warnings
-# warnings.filterwarnings("ignore")
 from main import main as evology
-# df,pop = evology("static", 'scholl', 'newton', False, 75, 0, 3, 0, [1/3, 1/3, 1/3], True, False)
 
 def job(iteration):
 	df,pop = evology("static", 'scholl', 'newton', False, 1000, 0, 10, 0, [1/3, 1/3, 1/3], True, False)
-	return df['WShare_NT'].iloc[-1]
-	# return random.randint(0,10)
+	return df['WShare_NT'], df['WShare_VI'], df['WShare_TF']
 
-# print(do_something())
-
+''' 
 reps = 30
 
 def main():
@@ -37,58 +31,34 @@ def main():
 
 if __name__ == '__main__':
 	main()
+	'''
+reps = 3
+def main():
+	start = time.perf_counter()
+	p = mp.Pool()
+	data = p.map(job, [i for i in range(reps)])
+	p.close()
+	print(data)
+	print(len(data))
+	data = np.array(list(data))
+	finish = time.perf_counter()
+	print(f'Multiprocessing () Finished in {round(finish-start, 2)} second(s)')
+	return data
 
+if __name__ == '__main__':
+	dataRun = main()
 
-
-# def main():
-# 	reps = 10
-# 	FinalResults = []
-# 	# with concurrent.futures.ProcessPoolExecutor() as executor:
-# 	with concurrent.futures.ProcessPoolExecutor() as executor:
-
-# 		results = [executor.submit(do_something) for _ in range(reps)]
-
-# 	for f in concurrent.futures.as_completed(results):
-# 		# print(f.result())
-# 		FinalResults.append(f.result())
-# 	# print(type(results))
-# 	del executor
-# 	return FinalResults
-
-# import multiprocessing as mp
-
-# def main(): 
-# 	reps = 10
-# 	FinalResults = []
-# 	processes = []
-# 	for _ in range(10):
-# 		p = mp.Process(target=do_something)
-# 		p.start()
-# 		processes.append(p)
-# 	for process in processes:
-# 		process.join()
-# 		FinalResults.append(queue.get())
-# 	return FinalResults
-	
-# '''
-# start = time.perf_counter()
-# if __name__ == '__main__':
-# 	print(main())
-# finish = time.perf_counter()
-# print(f'Multiprocessing Finished in {round(finish-start, 2)} second(s)')
-
-# def no_mp(reps):
-# 	results = []
-# 	for i in range(reps):
-# 		# results.append(random.randint(0,10))
-# 		results.append(do_something())
-# 	return results
-
-# start = time.perf_counter()
-# no_mp(reps)
-# finish = time.perf_counter()
-# print(f'Enumeration Finished in {round(finish-start, 2)} second(s)')
-# '''
-
-
-
+	print('----')
+	print(type(dataRun))
+	print(dataRun)
+	print(dataRun.shape)
+	''' 3 2 937
+	Hence 3 rows, 3 columns and each of the items is a list of 937 elements 
+	Because ATM the job outputs only 3 variables (columns): NT ([0]), VI ([1]), TF ([2]) 
+	And does 3 repetitions. Repetitions are rows.
+	Example: data from the first run is data[0,0] for the NT series, data[0,1] for VI series, data[0,2] for TF series. '''
+	plt.plot(dataRun[0,0], label = 'NT')
+	plt.plot(dataRun[0,1], label = 'VI')
+	plt.plot(dataRun[0,2], label = 'TF')
+	plt.legend()
+	plt.show()
