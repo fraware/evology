34ee66e7cae2c090cd19b585193e4b0edd17cc59
aymericvie <aymericvie78@gmail.com>
2022-01-05 16:16:27 +0100

    clean tests and add esl back

--- tests/test_learning.py
+++ tests/test_learning.py
@@ -3,13 +3,10 @@ from test_functions import *
 
 # learning_runs(repetitions, time, agents)
 
-print('Testing for 3 agents, 253 periods with 3 repetitions...')
-test_learning(3, 253, 3)
-print('...Succesful!')
+# print('Testing for 3 agents, 253 periods with 3 repetitions...')
+# test_learning(3, 253, 3)
+# print('...Succesful!')
 
-print('Testing for 3 agents, 253 periods with 3 repetitions and resetW True...')
-test_learning_reset(3, 253, 3)
-print('...Succesful!')
 
 print('Testing for 100 agents, 20,000 periods with 5 repetitions...')
 test_learning(5, 20000, 100)
