99505c91cfa9af0848ce4d7336fdd4027d0bd48a
aymericvie <aymericvie78@gmail.com>
2022-01-06 14:42:12 +0100

    deterministic test updated for learning

--- tests/test_deterministic.py
+++ tests/test_deterministic.py
@@ -1,5 +1,8 @@
 from test_functions import *
-print('Testing seed reproducibility for 100 agents, 10,000 periods with 10 repetitions...')
-test_det_pop_nolearning(10, 100, 100)
+print('Testing seed reproducibility for 100 agents, 1,000 periods with 10 repetitions, no learning...')
+test_det_pop_nolearning(10, 1000, 100)
 print('...Succesful!')
 
+print('Testing seed reproducibility for 100 agents, 1,000 periods with 10 repetitions, learning...')
+test_det_pop_learning(10, 1000, 100)
+print('...Succesful!')
