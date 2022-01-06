3b1ccb0ca65deaebdc581d7dc11caa1896a23c34
aymericvie <aymericvie78@gmail.com>
2022-01-06 14:40:42 +0100

    new deterministic test (nl)

---
+++ tests/test_deterministic.py
@@ -0,0 +1,5 @@
+from test_functions import *
+print('Testing seed reproducibility for 100 agents, 10,000 periods with 10 repetitions...')
+test_det_pop_nolearning(10, 100, 100)
+print('...Succesful!')
+
