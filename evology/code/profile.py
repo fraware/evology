0692c1bf84a2bf83c7fbb7391f1da3f2486cb3a7
aymericvie <aymericvie78@gmail.com>
2022-01-07 10:20:58 +0100

    profile work

--- evology/code/profile.py
+++ evology/code/profile.py
@@ -1,7 +1,7 @@
 import run
+import numpy as np
 import line_profiler
 profile = line_profiler.LineProfiler()
-import numpy as np
 np.random.seed(9)
 
 @profile
@@ -11,4 +11,9 @@ def simulation(POPULATION_SIZE, learning_mode, TIME, wealth_coordinates,):
 
 # simulation(100,0,1000,[1/3, 1/3, 1/3])
 
-''' In command: kernprof -v -l evology/code/profile.py > profile.txt ; no need to be in python env first'''
+''' In command: 
+kernprof -v -l evology/code/profile.py > evology/code/profile.txt
+kernprof -v -l evology/code/profile.py
+kernprof -v -l evology/code/run.py
+kernprof -v -l evology/code/run.py > evology/code/profile.txt
+ ; no need to be in python env first'''
