d75c953189d9adf7fa35e3ea52fcedf8a67935f1
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2022-01-17 17:00:46 +0000

    Update run.py

--- evology/code/run.py
+++ evology/code/run.py
@@ -2,17 +2,11 @@
 from main import *
 from parameters import *
 
-
 np.random.seed(8)
 wealth_coordinates = [1 / 3, 1 / 3, 1 / 3]
-# wealth_coordinates = [0.20899108903451205, 0.1210376286378561, 0.6699712823276319]
+# np.random.seed()
 # wealth_coordinates = np.random.dirichlet(np.ones(3), size=1)[0].tolist()
-
 print(wealth_coordinates)
-
-
-# main(mode, space, solver, circuit, MAX_GENERATIONS, PROBA_SELECTION, POPULATION_SIZE, MUTATION_RATE, wealth_coordinates, tqdm_display, reset_wealth):
-
 
 def run(
     POPULATION_SIZE, learning_mode, TIME, wealth_coordinates, tqdm_display, reset_wealth, ReinvestmentRate
@@ -108,7 +102,7 @@ def run(
 df, pop = run(
     100, 0, 10000, wealth_coordinates, tqdm_display=False, reset_wealth=False, ReinvestmentRate= 0
 )
-# df.to_csv("evology/data/run_data.csv")
+
 df.to_csv("evology/data/run_data.csv")
 print(df)
 print(stats.trim_mean(df['WealthAmp'], 0.1))
