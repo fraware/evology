1cfae16e7ff3d031626142f0482853dd46efb40b
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2022-02-24 17:12:32 +0000

    run

--- evology/code/run.py
+++ evology/code/run.py
@@ -40,20 +40,18 @@ def run(
 
 wealth_coordinates = np.random.dirichlet(np.ones(3), size=1)[0].tolist()
 np.random.seed(9)
-# wealth_coordinates = [1 / 3, 1 / 3, 1 / 3]
+wealth_coordinates = [1 / 3, 1 / 3, 1 / 3]
 np.random.seed(54)
-wealth_coordinates=[0.4538919915925864, 0.3758778035044387, 0.17023020490297489]
+# wealth_coordinates=[0.4538919915925864, 0.3758778035044387, 0.17023020490297489]
 print(wealth_coordinates)
 df, pop = run(
-    10, 0, 25000, wealth_coordinates, tqdm_display=False, reset_wealth=False, 
-    ReinvestmentRate = 1.1, InvestmentHorizon = 252, InvestorBehavior = 'profit')
+    3, 0, 50000, wealth_coordinates, tqdm_display=False, reset_wealth=False, 
+    ReinvestmentRate = 2.0, InvestmentHorizon = 5, InvestorBehavior = 'profit')
 
 df.to_csv("rundata/run_data.csv")
-print(df)
+# print(df)
 print([df['NT_returns'].mean(), df['VI_returns'].mean(), df['TF_returns'].mean()])
-obs = 500
-# diffreturn = (df['NT_returns'].tail(obs).mean() - df['VI_returns'].tail(obs).mean())**2 + (df['NT_returns'].tail(obs).mean() - df['TF_returns'].tail(obs).mean())**2 + (df['VI_returns'].tail(obs).mean()-df['TF_returns'].tail(obs).mean())**2
-# print(diffreturn)
-print(df['DiffReturns'].tail(100).mean())
-print
+print(df['DiffReturns'].tail(10000).mean())
+print(df['HighestT'].tail(10000).mean())
+
 
