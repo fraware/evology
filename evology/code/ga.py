3096ab482dd72350988799344fa09783ab7bd6a4
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2022-03-24 13:47:07 +0000

    bugfix: hyperM was not adding spoils

--- evology/code/ga.py
+++ evology/code/ga.py
@@ -69,7 +69,9 @@ def hypermutate(pop, spoils, replace):
         elif NumberReplace != 0:
             for index in index_to_replace:
                 half = CreateFractionalFund(pop, MaxFund, NumberReplace + 1)
+                spoils += pop[index].asset
                 del pop[index]
+                #print("Deleted " + str(ind.type) "; " + str(ind.wealth) + "; " + str(ind.asset))
                 pop.insert(index, half)
                 round_replacements += 1
             # FInally, add the last subdivision in place of the maximum fund.
