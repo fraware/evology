7c1ec6768a1c642e7c404fbcbda1ef42308ba11a
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2022-05-16 17:26:42 +0100

    test runs

--- evology/code/run.py
+++ evology/code/run.py
@@ -2,7 +2,7 @@ from main import *
 from parameters import *
 
 # wealth_coordinates = np.random.dirichlet(np.ones(3), size=1)[0].tolist()
-# np.random.seed(8)
+np.random.seed(8)
 wealth_coordinates = [1/3,1/3,1/3]
 print(wealth_coordinates)
 
@@ -13,7 +13,7 @@ df, pop, av_stats = main(
     space = 'extended', # 'extended',
     wealth_coordinates=wealth_coordinates,
     POPULATION_SIZE = 200,
-    MAX_GENERATIONS = 20 * 252, #20000, #1000 * 252,
+    MAX_GENERATIONS = 100 * 252, #20000, #1000 * 252,
     tqdm_display=False,
     reset_wealth=False,
 )
