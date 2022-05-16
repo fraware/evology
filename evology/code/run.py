f6dcb83fb37476949a43f59facdf96ddf5a375d0
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2022-05-16 12:29:30 +0100

    adjustements for stability

--- evology/code/run.py
+++ evology/code/run.py
@@ -2,17 +2,17 @@ from main import *
 from parameters import *
 
 # wealth_coordinates = np.random.dirichlet(np.ones(3), size=1)[0].tolist()
-np.random.seed(8)
+# np.random.seed(8)
 wealth_coordinates = [1/3,1/3,1/3]
 print(wealth_coordinates)
 
 def func(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, d, v, g, r):
     return 0
 df, pop, av_stats = main(
-    strategy = None, #func, #None, #func,
+    strategy = func, #None, #func,
     space = 'extended', # 'extended',
     wealth_coordinates=wealth_coordinates,
-    POPULATION_SIZE = 200,
+    POPULATION_SIZE = 201,
     MAX_GENERATIONS = 10000, #1000 * 252,
     tqdm_display=False,
     reset_wealth=False,
