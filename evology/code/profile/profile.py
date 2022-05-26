7aa2f7d2b91ca8438f8399a760e71b871a5cb8be
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2022-05-26 14:07:44 +0100

    profile

--- evology/code/profile/profile.py
+++ evology/code/profile/profile.py
@@ -7,37 +7,45 @@ from parameters import *
 import numpy as np
 
 @profile
+
 def main(
     strategy,
     space,
     wealth_coordinates,
     POPULATION_SIZE,
     MAX_GENERATIONS,
+    seed,
     tqdm_display,
     reset_wealth,
 ):
     # Initialisation
-    generation, CurrentPrice, dividend, spoils = 0, InitialPrice, INITIAL_DIVIDEND, 0
+    generation, CurrentPrice, dividend, spoils = 0, InitialPrice, INITIAL_DIVIDEND, 0.0
     results = np.zeros((MAX_GENERATIONS - data.Barr, data.variables))
-    price_history, dividend_history, replace, volume = [], [], 0, 0.0
+    dividend_history, replace, volume = [], 0, 0.0
+
+    # Random generator 
+    rng = np.random.default_rng(seed=seed)
+    np.random.seed(seed)
 
     # Population creation
-    pop, asset_supply = cr.CreatePop(POPULATION_SIZE, space, wealth_coordinates, CurrentPrice, strategy)
+    pop, asset_supply = cr.CreatePop(POPULATION_SIZE, space, wealth_coordinates, CurrentPrice, strategy, rng)
+
+    # Dividend and NT process generation
+    price_history = prc.FictiousPriceSeries(rng)
+    dividend_series, rd_dividend_series = div.ExogeneousDividends(MAX_GENERATIONS, rng)
+    rng = np.random.default_rng(seed=seed)
+    process_series = prc.ExogeneousProcess(MAX_GENERATIONS, rng)
 
     for generation in tqdm(
         range(MAX_GENERATIONS), disable=tqdm_display, miniters=100, mininterval=0.5
         ):
     #for generation in range(MAX_GENERATIONS):
 
-        if CurrentPrice >= 1_000_000:
-            warnings.warn('Simulation break: price above 1M.')
-            break
-
         # Population reset
-        pop = cr.WealthReset(pop, space, wealth_coordinates, generation, reset_wealth, CurrentPrice, strategy)
+        
+        pop = cr.WealthReset(pop, POPULATION_SIZE, space, wealth_coordinates, generation, reset_wealth, CurrentPrice, strategy, rng)
 
         # Hypermutation
-        
         pop, replacements, spoils = ga.hypermutate(
             pop, spoils, replace
         ) 
@@ -60,80 +68,41 @@ def main(
 
         # Market decisions 
         pop, replace = bsc.UpdateFullWealth(pop, CurrentPrice)
-        pop = bsc.NoiseProcess(pop)
+        #pop = bsc.NoiseProcess(pop, rng, process)
         pop = bsc.UpdateFval(pop, dividend)
-        pop = bsc.CalculateTSV_staticf(pop, price_history, dividend_history, CurrentPrice)
-        pop = bsc.CalculateTSV_avf(pop, generation, strategy, price_history, dividend)
-        #pop = bsc.DetermineEDF(pop)
-        
-
-        # Market clearing
-        #'''
-        #pop, mismatch, CurrentPrice, price_history, ToLiquidate = marketClearing(
-        #    pop, CurrentPrice, price_history, spoils, solver, volume
-        #)
-        #'''
-        
-
-        ''' For VI on previous price (scalar tsf)'''
-        # CurrentPrice = lc.linear_solver(pop, ToLiquidate, CurrentPrice)
+        pop = bsc.CalculateTSV_staticf(pop, price_history, dividend_history, CurrentPrice, process_series[generation])
+        pop = bsc.CalculateTSV_avf(pop, generation, strategy, price_history, dividend)        
         
         ToLiquidate = lc.DetermineLiquidation(spoils, volume)
 
         # ''' for VI on contemporaneous price ''' 
         # ed_functions = bsc.agg_ed_esl(pop, ToLiquidate)
-        # #CurrentPrice = float(esl_mc.esl_solver(ed_functions, CurrentPrice)[0])
-        # CurrentPrice = esl_mc.esl_solver(ed_functions, CurrentPrice)
+        
+        ed_functions = cz.agg_ed(pop, ToLiquidate)
 
-        ''' Test scipy '''
-        ''' for VI on contemporaneous price ''' 
-        ed_functions = bsc.agg_ed(pop, ToLiquidate)[0]
-        #CurrentPrice = float(esl_mc.esl_solver(ed_functions, CurrentPrice)[0])
-        CurrentPrice = esl_mc.scipy_solver(ed_functions, CurrentPrice)
-
-
-        # print(CurrentPrice)
-        # print(float(CurrentPrice[0]))
-        # print([type(price_history), type(CurrentPrice)])
-
-        price_history = lc.UpdatePriceHistory(price_history, CurrentPrice)
-        pop, mismatch = bsc.CalculateEDV(pop, CurrentPrice)
+        NewPrice = mc.scipy_solver(ed_functions, CurrentPrice)
+        pop, mismatch = cz.calculate_edv(pop, NewPrice)
 
         # Market activity
-        '''
-        (
-            pop,
-            volume,
-            dividend,
-            random_dividend,
-            dividend_history,
-            spoils,
-            Liquidations,
-        ) = mk.MarketActivity(
-            pop,
-            CurrentPrice,
-            asset_supply,
-            dividend,
-            dividend_history,
-            spoils,
-            ToLiquidate,
-            random_dividend_history
-        )
-        '''
-        dividend, random_dividend = mk.draw_dividend(dividend, random_dividend_history)
-        pop, volume, spoils, Liquidations = mk.execute_ed(pop, CurrentPrice, asset_supply, spoils, ToLiquidate)
+        dividend, random_dividend = dividend_series[0, generation], rd_dividend_series[0, generation]
+        pop, volume, spoils, Liquidations = mk.execute_ed(pop, NewPrice, asset_supply, spoils, ToLiquidate)
+
+        if volume != 0:
+            CurrentPrice = NewPrice
+        price_history = lc.UpdatePriceHistory(price_history, CurrentPrice)
+
         pop = mk.earnings(pop, dividend)
-        dividend_history.append(dividend)
         pop = mk.update_margin(pop, CurrentPrice)
         pop = mk.clear_debt(pop, CurrentPrice)
 
         pop, replace = bsc.UpdateWealthProfitAge(pop, CurrentPrice)
         pop = bsc.UpdateQuarterlyWealth(pop, generation)
         pop = bsc.UpdateWealthSeries(pop)
-
+        
+        '''
         if generation >= ShieldInvestment:
-            pop = iv.Emp_Investment(pop)
-        #AvgT, PropSignif, HighestT, AvgAbsT = 0, 0, 0, 0
+            pop = iv.Emp_Investment(pop, rng)
+        '''
 
         # Record results 
         results, sim_break = data.record_results(
@@ -149,32 +118,36 @@ def main(
             spoils,
             Liquidations,
             asset_supply,
+            process_series[generation]
         )
-
+        
         if sim_break == 1 and reset_wealth != True:
-            warnings.warn('Simulation break: one of the 3 strategy types is extinct.')
-            break
-
-    
+           warnings.warn('Only one base strategy left.')
+           break
 
     if generation < MAX_GENERATIONS - data.Barr:
         results = results[0:generation+1]
 
     df = pd.DataFrame(results, columns=data.columns)
 
-    av_stats = [df["AV_wealth"].iloc[-1] / df["AV_wealth"].iloc[0] - 1, df["AV_return"].mean(), df["AV_return"].std()]
+    # av_stats = [df["AV_wealth"].iloc[-1] / df["AV_wealth"].iloc[0] - 1, round(df["AV_return"].mean(),4), round(df["AV_return"].std(),3), 
+    # df["AV_wealth"].iloc[0], df["AV_wealth"].iloc[-1]]
 
-    return df, pop, av_stats
+
+    return df, pop #, av_stats
+
 
 np.random.seed(8)
 wealth_coordinates = [1 / 3, 1 / 3, 1 / 3]
-TIME, POPSIZE = 100000, 100
-df, pop, statsav = main(
+wealth_coordinates = [0.3, 0.5, 0.2]
+TIME, POPSIZE = 10_000, 1_000
+df, pop = main(
     None,
     "scholl",
     wealth_coordinates,
     POPSIZE,
     TIME,
+    8,
     False,
     False,
 )
--- evology/code/run.py
+++ evology/code/run.py
@@ -1,9 +1,10 @@
 from main import *
 from parameters import *
 
-seed = 10
-np.random.seed()
+seed = 8
+np.random.seed(8)
 wealth_coordinates = [1/3,1/3,1/3]
+wealth_coordinates = [0.3, 0.5, 0.2]
 # wealth_coordinates = [0.3, 0.3, 0.4]
 # wealth_coordinates=[0., 0., 1.]
 # wealth_coordinates = np.random.dirichlet(np.ones(3), size=1)[0].tolist()
@@ -16,8 +17,8 @@ df, pop = main(
     strategy = None, #func, #None, #func,
     space = 'scholl', # 'extended',
     wealth_coordinates = wealth_coordinates,
-    POPULATION_SIZE = 3,
-    MAX_GENERATIONS = 100000, #50 * 252, #20000, #1000 * 252,
+    POPULATION_SIZE = 1000,
+    MAX_GENERATIONS = 10000, #50 * 252, #20000, #1000 * 252,
     seed = seed,
     tqdm_display = False,
     reset_wealth = False,
