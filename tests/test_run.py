4f25a0877f5fa0f5cd32acb30f8f9f276868b7c1
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2022-05-27 15:16:28 +0100

    Update test_run.py

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 from test_functions import *
 
-print('[Scholl] Testing for 10 agents, 100,000 periods with 5 repetitions...')
-test_nolearning(5, 100_000, 10)
+print('[Scholl] Testing for 10 agents, 100,000 periods with 10 repetitions...')
+test_nolearning(10, 100_000, 10)
 print('...Succesful!')
 
-print('[Extended] Testing for 100 agents, 100,000 periods with 5 repetitions...')
-test_nolearning_ext(5, 100_000, 100)
+print('[Extended] Testing for 100 agents, 100,000 periods with 10 repetitions...')
+test_nolearning_ext(10, 100_000, 100)
 print('...Succesful!')
 
 
