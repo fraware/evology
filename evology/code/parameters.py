f41d11057ccd71cc2ba74503341b05538476ae65
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2022-05-27 13:57:54 +0100

    Update parameters.py

--- evology/code/parameters.py
+++ evology/code/parameters.py
@@ -76,7 +76,7 @@ min_vi_strat = 5
 max_vi_strat = 15
 
 min_tf_strat = 2
-max_tf_strat = 150 #252 #252
+max_tf_strat = 21 #252 #252
 
 # We coudl save a lot of runtime if TFs were in a discrete set of horizons a la Brock & Hommes
 max_strat_lag = max_tf_strat
