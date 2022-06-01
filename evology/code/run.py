0c5455465875d94227f1938cfd954476df3d8db8
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2022-06-01 14:10:59 +0100

    Update run.py

--- evology/code/run.py
+++ evology/code/run.py
@@ -1,13 +1,8 @@
 from main import *
 from parameters import *
 
-
 wealth_coordinates = [1/3,1/3,1/3]
-# wealth_coordinates = [0.15, 0.6, 0.25]
-# wealth_coordinates = [0.3, 0.3, 0.4]
-# wealth_coordinates=[0.37469673478000054, 0.21665619652962376, 0.40864706869037576]
 # wealth_coordinates = np.random.dirichlet(np.ones(3), size=1)[0].tolist()
-# wealth_coordinates=[0.3877491723252324, 0.439903019419811, 0.17234780825495657]
 seed = 9
 np.random.seed(seed)
 print(wealth_coordinates)
@@ -28,7 +23,3 @@ df, pop = main(
 print(df)
 df.to_csv("rundata/run_data.csv")
 print(df["WShare_NT"].iloc[-1], df["WShare_VI"].iloc[-1], df["WShare_TF"].iloc[-1])
-
-# for ind in pop:
-#     if ind.type == 'vi':
-#         print([ind.strategy, ind.val, ind.val_net])
