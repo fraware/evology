1ac6a93882861516a866beeed283ba370f159fa7
Aymeric Vié <39515571+aymericvie@users.noreply.github.com>
2022-06-14 11:32:40 +0100

    test for oop mini model

--- evology/code/oop/main.py
+++ evology/code/oop/main.py
@@ -1,9 +1,23 @@
 from simulation import Simulation
 
-if __name__ == "__main__":
+def main(
+    max_generations, 
+    population_size,
+    interest_rate,
+    seed
+    ):
     s = Simulation(
+        max_generations = max_generations, 
+        population_size = population_size,
+        interest_rate=interest_rate,
+        seed = seed)
+    s.simulate()
+
+if __name__ == "__main__":
+    main(
         max_generations = 10000, 
         population_size = 3,
-        interest_rate=0.01,
-        seed = 1)
-    s.simulate()
+        interest_rate = 0.01,
+        seed = 0
+    )    
+    
---
+++ tests/oop.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append('./evology/code/oop')
+import numpy as np
+from main import main as model
+
+def runs(repetitions, time, agents):
+    went_smoothly = True
+    i = 0
+    while i < repetitions:
+        try: 
+            seed = np.random.randint(0,100)
+            np.random.seed(seed)
+            df,pop = model(agents, time, 0.01, seed)
+        except Exception as e: 
+            went_smoothly = False
+            print('Seed ' + str(seed))
+            print('Process ' + str(i) + ' encoutered an exception.')
+            print(str(e))
+            break
+        i += 1
+    return went_smoothly
+
+def test_runs(repetitions, time, agents):
+    went_smoothly = runs(repetitions, time, agents)
+    assert went_smoothly == True
+
+test_runs(10, 100_000, 3)
