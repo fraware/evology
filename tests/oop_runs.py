74449810a7c8928406220157e9112f38b95f8ca5
aymericvie <aymericvie78@gmail.com>
2022-09-01 09:46:34 +0200

    reduce IC scope of test

--- tests/oop_runs.py
+++ tests/oop_runs.py
@@ -22,6 +22,8 @@ def job1(param):
     try: 
         seed = np.random.randint(0,100000)
         wealth_coords = np.random.dirichlet(np.ones(3), size=1)[0].tolist()
+        while wealth_coords[2] > 1/3:
+            wealth_coords = np.random.dirichlet(np.ones(3), size=1)[0].tolist()
         np.random.seed(seed)
         inv_bool = random_bool()
         reset_bool = random_bool()
